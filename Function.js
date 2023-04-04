de9257b02dbabe26e7c1527f5814ff2badb7dd40
atabursoft90 <129271794+atabursoft90@users.noreply.github.com>
2023-04-04 11:35:11 +0600

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,92 @@
+/*function nam(){
+    let a =30;
+    let b =70;
+    let sum = a+b;
+    console.log("sum:",sum);
+}
+ nam() */ 
+
+ 
+ /*function fun (){
+    let a = 60;
+    let b = 50;
+    let sum = a*b;
+    console.log(sum)
+ }
+  fun()*/
+  /*function fun (){
+    let a = 60;
+    let b = 50;
+    let sum = a-b;
+    console.log(sum)
+ }
+  fun()*/
+
+
+
+ /* function nam (a,b){
+        let total = a+b;
+        console.log(total)
+  }
+   nam(60,90)
+
+
+ //* function nam (x,y){
+    let  sum = x*y;
+    console.log(sum)
+}
+ nam(50,90)
+ nam(50,90)*/
+
+
+
+
+ /*function nam (x){
+    let total= x* 20;
+    return total;
+ }
+  var a =nam(20);
+  console.log(a)*/
+
+
+
+ /*function tumi(a){
+    let sum = a+200;
+    return sum;
+ }
+ let x = tumi(20);
+ let y = tumi(30);
+ let z = tumi (50)
+ console.log(x)
+ console.log(y)
+ console.log(z) */
+ 
+
+/*function teacher(a) {
+    let sum = a+100;
+    return sum;
+ }
+ let x = teacher(10);
+ let y = teacher(10);
+ let z = teacher(10);
+ console.log(x+y+z)*/
+
+ /*function teacher(n){
+      let sum = n+200;
+      return sum;
+ }
+ let  a = teacher(10);
+ let  b = teacher(20);
+ let  c = teacher(30);
+  console.log(a*b*c)*/
+ 
+
+/*function teacher(n){
+      let sum = n+200;
+      return sum;
+ }
+ let  a = teacher(10);
+ let  b = teacher(20);
+ let  c = teacher(30);
+  console.log(a/b/c)*/
+ 
